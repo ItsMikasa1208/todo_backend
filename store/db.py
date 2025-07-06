de76e53b60e41260833a8b369ec7676f5b93812d
Prasanth Raj <praj496359@gmail.com>
2025-07-06 12:46:38 +0530

    supabase and mongodb connection partially done

--- store/db.py
+++ store/db.py
@@ -2,7 +2,7 @@ from typing import Optional
 from pymongo import AsyncMongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
-
+from core.config import settings
 
 class Database:
     client : AsyncMongoClient = None
@@ -13,3 +13,24 @@ class Database:
     notes : Collection
     priority : Collection
     status : Collection
+
+db = Database()
+
+async def connect_db():
+    try:
+        if not settings.MONGODB_URI:
+            raise("MongoDB URI isn't defined!!!")
+        
+        db.client = AsyncMongoClient(settings.MONGODB_URI)
+        db.database = db.client.get_database("notes")
+        
+        #Collections
+        db.user = db.database["user"]
+        db.token = db.database["token"]
+        db.notes = db.database["notes"]
+        db.priority = db.database["priority"]
+        db.status = db.database["status"]
+
+
+    except Exception as e:
+        print(e)
--- store/storage.py
+++ store/storage.py
@@ -1,5 +1,6 @@
 from supabase import create_client, Client
+from core.config import settings
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = settings.SUPABASE_S3_URL
+key: str = settings.SUPABASE_S3_SECRET_KEY
 supabase: Client = create_client(url, key)
